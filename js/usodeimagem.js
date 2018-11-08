250ddee850908c034978a347a2d3924e139cc753
PriscilaCalisto01 <43457491+PriscilaCalisto01@users.noreply.github.com>
2018-11-08 21:19:11 -0200

    Update usodeimagem.js

--- js/usodeimagem.js
+++ js/usodeimagem.js
@@ -90,3 +90,20 @@ function registrarNomeEmpresa() {
         }
     });
 }
+
+function registrarNomeAgente() {
+	var statusTransacao = document.getElementById("statusTransacaoNomeAgente");
+	var nomeAgente = document.formNomeAgente.campoNomeAgente.value;
+	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
+	contratoUsoDeImagem.definirAgente(nomeAgente, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        if (err)    {
+            console.log("Erro");
+			console.error(err);
+			statusTransacao.innerHTML = "Erro: " + err;
+        } else {
+            console.log("Resultado");
+            console.log(resultado);
+            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
+        }
+    });
+}
