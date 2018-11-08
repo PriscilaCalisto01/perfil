f823eebadfe1142b710db875329cdc3d23fe035e
PriscilaCalisto01 <43457491+PriscilaCalisto01@users.noreply.github.com>
2018-11-08 21:48:46 -0200

    Update usodeimagem.js

--- js/usodeimagem.js
+++ js/usodeimagem.js
@@ -92,10 +92,10 @@ function registrarNomeEmpresa() {
 }
 
 function definirAgente() {
-	var statusTransacao = document.getElementById("statusTransacaoNomeAgente");
-	var definirAgente = document.formNomeAgente.campoNomeAgente.value;
+	var statusTransacao = document.getElementById("statusTransacaoDefinirAgente");
+	var definirAgente = document.formDefinirAgente.campoDefinirAgente.value;
 	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
-	contratoUsoDeImagem.definirAgente(nomeAgente, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+	contratoUsoDeImagem.definirAgente(DefinirAgente, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
         if (err)    {
             console.log("Erro");
 			console.error(err);
